523783c93665e5793c310b5f757bb816fba5eb21
lygris <lygriz@gmail.com>
2026-03-07 15:52:57 -0600

    fix: correct temperature unit mapping (2=Celsius, 1=Fahrenheit)

--- components/dreo_heater/dreo_heater.h
+++ components/dreo_heater/dreo_heater.h
@@ -45,8 +45,8 @@ class DreoHeater : public climate::Climate, public uart::UARTDevice, public Comp
   void set_unit_switch(switch_::Switch *s) { unit_switch = s; temp_unit_switch = s; }
 
   void set_temp_unit(bool is_celsius) {
-    // 0x16 type 04 length 01. 0x01=°C, 0x02=°F (common Tuya convention)
-    send_tuya_dp(0x16, 0x04, 1, {(uint8_t)(is_celsius ? 1 : 2)});
+    // 0x16 type 04 length 01. 0x02=°C, 0x01=°F
+    send_tuya_dp(0x16, 0x04, 1, {(uint8_t)(is_celsius ? 2 : 1)});
   }
   void set_fahrenheit(bool is_fahrenheit) { set_temp_unit(!is_fahrenheit); }
 
@@ -356,7 +356,7 @@ class DreoHeater : public climate::Climate, public uart::UARTDevice, public Comp
               case 16: if (child_lock_switch) child_lock_switch->publish_state(val != 0); break;
               case 20: if (window_switch) window_switch->publish_state(val != 0); break;
               case 22: // 0x16 temp unit
-                if (unit_switch) unit_switch->publish_state(val == 1); // 1=Celsius, 2=Fahrenheit
+                if (unit_switch) unit_switch->publish_state(val == 2); // 2=Celsius, 1=Fahrenheit
                 break;
               default: break;
           }
